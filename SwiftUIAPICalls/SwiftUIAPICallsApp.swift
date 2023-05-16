b18e5fa0fe161ed59ca5cf05383f6683fed555d4
jonsalkin <50082962+jonsalkin@users.noreply.github.com>
2023-05-16 18:00:55 -0400

    Initial Commit

---
+++ SwiftUIAPICalls/SwiftUIAPICallsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAPICallsApp.swift
+//  SwiftUIAPICalls
+//
+//  Created by Jon Salkin on 5/16/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAPICallsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
