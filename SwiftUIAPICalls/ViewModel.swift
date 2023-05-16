3c88865664c85c2eead286428b6aa75e601dcff2
jonsalkin <50082962+jonsalkin@users.noreply.github.com>
2023-05-16 19:47:45 -0400

    Initial commit.

---
+++ SwiftUIAPICalls/ViewModel.swift
@@ -0,0 +1,46 @@
+//
+//  ViewModel.swift
+//  SwiftUIAPICalls
+//
+//  Created by Jon Salkin on 5/16/23.
+//
+
+import Foundation
+import SwiftUI
+
+struct Course: Hashable, Codable {
+    let name: String
+    let image: String
+}
+
+class ViewModel: ObservableObject {
+    @Published var courses: [Course] = []
+    
+    func fetch() {
+        guard let url = URL(string: "https://iosacademy.io/api/v1/courses/index.php") else {
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) {[weak self] data, _, error in
+            guard let data = data, error == nil else {
+                return
+            }
+            
+            //Convert to JSON
+            do {
+                let courses = try
+                JSONDecoder().decode([Course].self, from: data)
+                DispatchQueue.main.async {
+                    self?.courses = courses
+                }
+            }
+            catch {
+                print(error)
+            }
+        }
+        
+        task.resume()
+    }
+}
+
+
